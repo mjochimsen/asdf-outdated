4a156d1cae3b4ff8f0d83e29d330376745780904
Michael Jochimsen <mlj@troglodytics.org>
2018-11-28 22:50:03 -0800

    Add caching to `Outdated::ASDF`.
    Cache the values we get from the `Outdated::ASDF` methods to minimize
    their performance impact.

--- src/asdf.cr
+++ src/asdf.cr
@@ -3,22 +3,39 @@
 module Outdated::ASDF
   extend self
 
+  @@plugins : Array(String)? = nil
+
+  @@all_versions = Hash(String, Array(Version)).new do |versions, plugin|
+    unless versions.has_key?(plugin)
+      versions[plugin] = `asdf list-all #{plugin}`.split
+        .compact_map { |version| Version.parse?(version) }
+    end
+    versions[plugin]
+  end
+
+  @@installed_versions = Hash(String, Array(Version)).new do |versions, plugin|
+    unless versions.has_key?(plugin)
+      versions[plugin] = `asdf list #{plugin}`.split
+        .compact_map { |version| Version.parse?(version) }
+    end
+    versions[plugin]
+  end
+
   # Return a list of the `adsf` plugins installed on the system.
   def plugins : Array(String)
-    `asdf plugin-list`.split
+    @@plugins = `asdf plugin-list`.split if @@plugins.nil?
+    @@plugins.not_nil!
   end
 
   # Return a list of all the versions supported by a given `asdf`
   # *plugin*.
   def list_all(plugin : String) : Array(Version)
-    `asdf list-all #{plugin}`.split
-      .compact_map() { |version| Version.parse?(version) }
+    @@all_versions[plugin]
   end
 
   # Return a list of all the installed versions for a given `asdf`
   # *plugin*.
   def list_installed(plugin : String) : Array(Version)
-    `asdf list #{plugin}`.split
-      .compact_map { |version| Version.parse?(version) }
+    @@installed_versions[plugin]
   end
 end
