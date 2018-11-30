aafe27d65a7100e666fb6547ac86a32d94f63a2a
Michael Jochimsen <mlj@troglodytics.org>
2018-11-29 18:48:28 -0800

    Hook up an entrypoint for the application.
    The entrypoint will only be run when were not in test mode (when the
    `Spec::ObjectExtensions` are not present). Right now it doesn't do
    anything, but we expect that to change soon.

--- src/outdated.cr
+++ src/outdated.cr
@@ -1,7 +1,16 @@
 require "./asdf"
 require "./version"
 
-# TODO: Write documentation for `Outdated`
 module Outdated
   VERSION = "0.1.0"
+
+  # Get a list of outdated `asdf` plugins and the possible new versions.
+  def self.run(plugins : Array(String))
+  end
+end
+
+# Never run if we're under test.
+unless nil.responds_to?(:should)
+  args = ARGV.empty? ? Outdated::ASDF.plugins : ARGV
+  Outdated.run(args)
 end
