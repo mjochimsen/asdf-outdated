bfbacca996c57739c6e689afd4736546627c272d
Michael Jochimsen <mlj@troglodytics.org>
2018-11-30 15:09:07 -0800

    Get list of outdated versions.
    Outdated versions are found for each plugin provided to
    `Outdated::run()`. We find the outdated version (and its upgrade) by
    seeing if there are any newer versions available, and then taking the
    closest one.
    
    We would like to eliminate versions for which we already have a version
    installed from the same series, but that's a harder problem.

--- src/outdated.cr
+++ src/outdated.cr
@@ -6,6 +6,18 @@ module Outdated
 
   # Get a list of outdated `asdf` plugins and the possible new versions.
   def self.run(plugins : Array(String))
+    plugins.each do |plugin|
+      ASDF.list_installed(plugin).each do |installed|
+        upgrade = upgrade?(plugin, installed)
+        puts "#{plugin} #{installed} (#{upgrade})" unless upgrade.nil?
+      end
+    end
+  end
+
+  private def self.upgrade?(plugin, installed)
+    ASDF.list_all(plugin)
+      .select { |version| version > installed }
+      .min?
   end
 end
 
